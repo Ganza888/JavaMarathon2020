ac88a8af0286c4d0424832c64518d5ceb209349c
Kirill Kunitski <slevinmylo@gmail.com>
2020-12-25 22:15:29 +0300

    complete day2 tasks

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int i = a;
+        while (i < b) {
+            i++;
+            if (i < b && i % 5 == 0 && i % 10 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
