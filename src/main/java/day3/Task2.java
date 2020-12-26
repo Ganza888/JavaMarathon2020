4f8162a61393ee2c39be3233c0bfbada457228ed
Kirill Kunitski <slevinmylo@gmail.com>
2020-12-26 23:47:20 +0300

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            double a = s.nextDouble();
+            double b = s.nextDouble();
+            double div = a / b;
+            if (b > 0) {
+                System.out.println(div);
+            }
+            if ( b == 0)
+                break;
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+            for(int i = 0; i < 5; i++) {
+                double a = s.nextDouble();
+                double b = s.nextDouble();
+                double div = a / b;
+                if (b > 0) {
+                    System.out.println(div);
+                }
+                if ( b == 0) {
+                    System.out.println("Деление на 0");
+                }
+            }
     }
 }
