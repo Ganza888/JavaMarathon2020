0260bf01fba12521e7196a6659d6458aff6cb762
Kirill Kunitski <slevinmylo@gmail.com>
2020-12-28 14:59:03 +0300

    correct task1 day3

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -5,11 +5,7 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String r = "Россия";
-        String i = "Италия";
-        String e = "Англия";
-        String g = "Германия";
-        String u = "Неизвестная страна";
+        String country;
         while (true) {
             String city = s.nextLine();
             if (city.equals("Stop"))
@@ -18,27 +14,28 @@ public class Task1 {
                 case "Москва" :
                 case "Владивосток" :
                 case "Ростов" :
-                    System.out.println(r);
+                    country = "Россия";
                     break;
                 case "Рим" :
                 case "Милан" :
                 case "Турин" :
-                    System.out.println(i);
+                    country  = "Италия";
                     break;
                 case "Ливерпуль" :
                 case "Манчестер" :
                 case "Лондон" :
-                    System.out.println(e);
+                    country = "Англия";
                     break;
                 case "Берлин" :
                 case "Мюнхен" :
                 case "Кёльн" :
-                    System.out.println(g);
+                    country = "Германия";
                     break;
                 default:
-                    System.out.println(u);
+                    country = "Неизвестная страна";
                     break;
             }
+            System.out.println(country);
         }
     }
 }
