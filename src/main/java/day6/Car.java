f6fdf37f36fcc9cc7f108b040255958f689fbf8d
Kirill Kunitski <slevinmylo@gmail.com>
2020-12-30 00:21:39 +0300

    correct task3 day6

--- src/main/java/day6/Car.java
+++ src/main/java/day6/Car.java
@@ -46,6 +46,6 @@ public class Car {
         } else {
             System.out.println("Неверное значение");
         }
-        return yearDifference;
+        return year;
     }
 }
--- src/main/java/day6/Task3.java
+++ src/main/java/day6/Task3.java
@@ -10,17 +10,22 @@ public class Task3 {
 class Teacher {
     private String name;
     private String subject;
-    String grade;
+    private String grade;
 
     public Teacher(String name, String subject) {
         this.name = name;
         this.subject = subject;
     }
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+    public String getGrade() {
+        return grade;
+    }
 
     public void evaluate(Student student) {
         int gradeInt = (int) (Math.random() * (6 - 2) + 2);
-        System.out.println("Преподаватель " + name + " оценил студента с именем " +
-                student.name + " по предмету " + subject + " на оценку " + grade);
+
         if (gradeInt == 2) {
             grade = "неудовлетворительно";
         }
@@ -33,14 +38,17 @@ class Teacher {
         if (gradeInt == 5) {
             grade = "отлично";
         }
-
-
+        System.out.println("Преподаватель " + name + " оценил студента с именем " +
+                student.getName() + " по предмету " + subject + " на оценку " + grade);
     }
 }
 class Student {
-    String name;
+    private String name;
 
     public Student(String name) {
         this.name = name;
+    }
+    public String getName() {
+        return name;
     }
 }
