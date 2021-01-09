15bbd5bfda48eb9d862aac8acf91eb463386f9c7
Kirill Kunitski <slevinmylo@gmail.com>
2021-01-09 20:12:21 +0300

    complete day13 task

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,27 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("n1");
+        User user2 = new User("n2");
+        User user3 = new User("n3");
 
+        user1.sendMessage(user2, "Hola");
+        user1.sendMessage(user2, "Que tal");
+
+        user2.sendMessage(user1, "Hi");
+        user2.sendMessage(user1, "Whats up");
+        user2.sendMessage(user1, "Bye");
+
+        user3.sendMessage(user1, "asd");
+        user3.sendMessage(user1, "dsa");
+        user3.sendMessage(user1, "zxc");
+
+        user1.sendMessage(user3, "qwe");
+        user1.sendMessage(user3, "ert");
+        user1.sendMessage(user3, "dfg");
+
+        user3.sendMessage(user1, "cvb");
+
+        MessageDatabase.showDialog(user1, user3);
     }
 }
