6ed33e7f2c4e71cb68f8d134d4aa6f1a94957342
Kirill Kunitski <slevinmylo@gmail.com>
2021-01-10 18:38:34 +0300

    complete day14 tasks

--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,35 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(parseFileToStringList());
+    }
 
+    public static List<String> parseFileToStringList() {
+        File file = new File("People.txt");
+        List<String> list = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] people = line.split(" ");
+                if (Integer.parseInt(people[1]) < 0) {
+                    throw new IllegalArgumentException();
+                }
+                list.add(line);
+            }
+            return list;
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return null;
     }
 }
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,36 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        System.out.println(parseFileToObjList());
+    }
 
+    public static List<Person> parseFileToObjList() {
+        File file1 = new File("People.txt");
+        List<Person> list = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file1);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] people = line.split(" ");
+                if (Integer.parseInt(people[1]) < 0) {
+                    throw new IllegalArgumentException();
+                }
+                Person person1 = new Person(people[0],Integer.parseInt(people[1]));
+                list.add(person1);
+            }
+            return list;
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return null;
     }
 }
