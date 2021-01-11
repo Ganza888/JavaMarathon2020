6838b4dcf60b781db4bbbede6e88057ff983d122
Kirill Kunitski <slevinmylo@gmail.com>
2021-01-11 21:39:59 +0300

    complete day16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,27 @@
 package day16;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("t1.txt");
+        printResult(file);
+    }
+
+    public static void printResult(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        String[] numbers = line.split(" ");
+        double sum = 0;
+        double average = 0;
+        for (String x : numbers) {
+            sum += Integer.parseInt(x);
+            average = sum / numbers.length;
+        }
+        System.out.printf(average + " --> %.3f", average);
+        scanner.close();
     }
 }
 
